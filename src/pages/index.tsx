f3907b661fd138dc33d4dfad2126d4f65a53523c
melvin.php.ph <melvin.php.ph@tripleonetech.net>
2023-09-04 21:12:13 +0800

    router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,8 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -16,10 +16,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {!user.isSignedIn && <SignInButton/>}
-        {!!user.isSignedIn && <SignOutButton/>}
-        <SignUpButton />
-        <UserButton afterSignOutUrl="/"/>
+        <div>
+            {!user.isSignedIn && <SignInButton/>}
+            {!!user.isSignedIn && <SignOutButton/>}
+            <SignUpButton />
+            <UserButton afterSignOutUrl="/"/>
+        </div>
+        <div>
+            { data?.map((post) => (
+                <div key={post.id}>
+                    {post.content}
+                </div>
+            )) }
+        </div>
       </main>
     </>
   );
